b333c9810a0cfe6ca98023e2d6a8305f34147537
Darren Chu <darren.cw.chu@gmail.com>
2024-09-14 17:33:01 -0400

    upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import { useLocation } from 'react-router-dom'
 import { Routes } from './router/Router'
 import Navbar from '@components/Navbar'
 import { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 import './App.scss'
+import { auth } from './firebase/client'
 
 function App() {
   const location = useLocation()
   const [showNavbar, setShowNavbar] = useState(true)
+  const [user] = useAuthState(auth)
   useEffect(() => {
     if (location.pathname === '/login' || location.pathname === '/register') {
       setShowNavbar(false)
@@ -18,7 +21,7 @@ function App() {
   }, [location.pathname])
   return <>
     { showNavbar && <Navbar />}
-    <Routes />
+    <Routes signedIn={user?true:false} />
   </>
 }
 
