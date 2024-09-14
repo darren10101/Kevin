127f843448f968316e2e31bb266bfedc3e7a1199
Darren Chu <darren.cw.chu@gmail.com>
2024-09-14 09:23:30 -0400

    updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Routes } from './router/Router'
 import Navbar from '@components/Navbar'
+import { useEffect } from 'react'
 
 import './App.css'
 
 function App() {
-
+  useEffect(() => {
+    document.title = 'Frontend Kevin'
+  }, [])
   return (
     <BrowserRouter basename='/'>
       <Navbar />
