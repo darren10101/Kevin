d49040069261f53a8651a2bdf9aafe6a6222e424
Isolumi <hubertx98@gmail.com>
2024-09-14 21:03:44 -0400

    temp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,46 @@
-import React, { useEffect, useState, useRef} from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { useLocation } from 'react-router-dom'
+import { Routes } from './router/Router'
+import Navbar from '@components/Navbar'
+import axios from 'axios'
+import './App.scss'
 
 const App: React.FC = () => {
+    const location = useLocation()
+    const [showNavbar, setShowNavbar] = useState(true)
+    const [path, setPath] = useState('')
+    const [user, setUser] = useState(true)
+    useEffect(() => {
+        const checkAuth = async () => {
+            const token = localStorage.getItem('token');
+            if (token) {
+                try {
+                    const response = await axios.get('http://localhost:5000/user/verify', {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    });
+                    if (response.status != 200) {
+                        setUser(false)
+                    }
+                } catch (error) {
+                    console.error('Error verifying token:', error);
+                }
+            }
+        };
+        checkAuth();
+    }, []);
+
+    useEffect(() => {
+        if (location.pathname === '/login' || location.pathname === '/register') {
+            setShowNavbar(false)
+        } else {
+            setShowNavbar(true)
+        }
+        setPath(location.pathname)
+        document.title = 'Frontend Kevin'
+    }, [location.pathname])
+    // ==================== Voice Recognition ====================
     const [transcript, setTranscript] = useState<string>("")
     const [listening, setListening] = useState<boolean>(false)
     const [isCapturing, setIsCapturing] = useState<boolean>(false)
@@ -101,21 +141,10 @@ const App: React.FC = () => {
         };
     }, []);
 
-    return (
-        <div>
-            <h1>Voice Listener</h1>
-            <p>Say something, I am listening!</p>
-            <div>
-                <strong>Transcript:</strong> {transcript}
-            </div>
-            <button onClick={startListening} disabled={listening}>
-                Start Listening
-            </button>
-            <button onClick={stopListening} disabled={!listening}>
-                Stop Listening
-            </button>
-        </div>
-    );
+    return <>
+        {showNavbar && <Navbar path={path} />}
+        <Routes signedIn={user} />
+    </>
 };
 
 export default App;
